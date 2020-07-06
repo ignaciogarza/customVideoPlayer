01ac6036047c3e6c35060cab79da71e6726a8c5a
Ignacio Garza <ignacioagarza@gmail.com>
2020-07-06 11:46:51 -0500

    Basic HTML and CSS layout structure define

---
+++ script.js
@@ -0,0 +1,18 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play and pause video
+
+// event listener
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
